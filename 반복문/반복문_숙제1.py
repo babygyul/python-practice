268d546db4dff0431461da560e173b42cb6a5107
unknown <roehd2780@naver.com>
2022-10-23 15:02:43 +0900

    반복문 숙제 완

--- 반복문/반복문_숙제1.py
+++ 반복문/반복문_숙제1.py
@@ -1,9 +1,37 @@
+
 # Q1. 0부터 99까지의 합을 출력하세요
+
+sum= 0
+for i in range(100):
+    sum = sum + i
+print(sum)
 
 # Q2. 0부터 100까지의 합을 출력하세요
 
+sum = 0
+for i in range(101):
+    sum = sum + i
+print(sum) 
+
 # Q3. 0부터 1000까지의 합을 출력하세요
+
+sum = 0
+for i in range(1001):
+    sum= sum + i
+print(sum)
+
 
 # Q4. 0부터 999까지의 합을 출력하세요
 
+sum=0
+for i in range(1000):
+    sum = sum+i
+print(sum)
+
 # Q5. 0부터 100까지의 합이 출력되는 과정을 전부 출력하세요
+
+sum = 0
+for i in range(101):
+    sum = sum + i
+    print(sum)
+    
--- 반복문/반복문_연습.py
+++ 반복문/반복문_연습.py
@@ -2,5 +2,6 @@
 
 sum = 0
 
-for i in range(100):
+for i in range(1000):
     sum = sum + i
+    print(i)
